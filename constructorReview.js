2387e044ab71c257de34a57c831c7b469d15c8e6
Ben Pluim <benpluim@Bens-MacBook-Pro.local>
2017-06-21 20:52:10 -0600

    JS Review

--- constructorReview.js
+++ constructorReview.js
@@ -1,21 +1,35 @@
 //Create a Animal Constructor that has the following parameters. species, name, legs, color, food (which is an array of foods they can eat).
 
   //code here
+var Animal = function(species, name, legs, color, food) {
+  this.species = species;
+  this.name = name;
+  this.legs = legs;
+  this.color = color;
+  this.food = food;
+}
 
 
 //Now create a person function that creates an object and returns it (not in constructor form) that has the following parameters. name, age, height, gender
 
   //code here
 
+function person() {
+  return {name, age, height, gender};
+}
+
 
 //Create a animal array and a person array.
 
   //code here
+var animal = ['dog', 'German Shepherd', 4, 'brown', ['meat', 'dogfood']];
+var person = ['homsapien', 'Ben', 2, 'white', ['vegetables', 'water']];
 
 
 //Create two instances of Animal and push those into your animal array
 
   //code here
+var germanShepherd = new Animal()
 
 
 //Create two instances of person and push those into your person array.
